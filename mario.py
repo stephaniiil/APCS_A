a2b6bb355d1ff332f112afd18a0a09ff308bf29d
stephaniiil <142924483+stephaniiil@users.noreply.github.com>
2024-01-26 13:45:08 -0800

    Create mario.py

---
+++ mario.py
@@ -0,0 +1,18 @@
+num = input("Enter an integer height between 1 to 8: ")
+while not num.isdigit():  # reject non integer
+    num = input("Enter an integer height between 1 to 8: ")
+height = int(num)
+while height < 1 or height > 8:
+    num = input("Enter an integer height between 1 to 8: ")
+    while not num.isdigit():  # reject non integer
+        num = input("Enter an integer height between 1 to 8: ")
+    height = int(num)
+for length in range(1, height + 1):
+    for hashtag in range(height - length):
+        print(" ", end="")
+    for hashtag in range(length):
+        print("#", end="")
+    print("  ", end="")
+    for hashtag in range(length):
+        print("#", end="")
+    print()
